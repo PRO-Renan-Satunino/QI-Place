54b4fd96fc7c9a8509ff2d4ae1b0cf472b3a5e59
Renan J. Saturnino <161033810+PRO-Renan-Satunino@users.noreply.github.com>
2024-04-24 09:00:19 -0300

    Add files via upload

---
+++ sitezin/script.js
@@ -0,0 +1,15 @@
+// Função para o slider de destaques
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("destaque-slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Altera a imagem a cada 3 segundos (3000 milissegundos)
+}
